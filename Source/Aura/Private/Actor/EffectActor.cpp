e770f9378c176155b5465dd9373d56d3026307f7
likemango <330857910@qq.com>
2024-06-07 14:42:56 +0800

    实现测试用的EffectAcor能够修改角色属性

---
+++ Source/Aura/Private/Actor/EffectActor.cpp
@@ -0,0 +1,64 @@
+﻿// Copyright XiaoYao.
+
+
+#include "Actor/EffectActor.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Aura/Aura.h"
+#include "Character/AuraCharacter.h"
+#include "Components/SphereComponent.h"
+
+AEffectActor::AEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	SetRootComponent(Mesh);
+	Sphere = CreateDefaultSubobject<USphereComponent>("SphereComp");
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	Sphere->SetupAttachment(Mesh);
+}
+
+void AEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AEffectActor::OnBeginOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AEffectActor::OnEndOverlap);
+}
+
+void AEffectActor::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(OtherActor)
+	{
+		UE_LOG(LogAura, Warning, TEXT("OnBeginOverlap, hit actor: %s"), *OtherActor->GetName());
+
+		// if(AAuraCharacterBase* AuraCharacterBase = Cast<AAuraCharacterBase>(OtherActor))
+		// {
+		// 	UAuraAttributeSet* AttributeSet = Cast<UAuraAttributeSet>(AuraCharacterBase->GetAttributeSet());
+		// 	AttributeSet->SetHealth(80);
+		// }
+
+		if(IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+		{
+			const UAuraAttributeSet* AttributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+			UAuraAttributeSet* MutableAttributeSet = const_cast<UAuraAttributeSet*>(AttributeSet);
+			UE_LOG(LogAura, Warning, TEXT("Health = %f"), AttributeSet->GetHealth())
+			MutableAttributeSet->SetHealth(40.f);
+			UE_LOG(LogAura, Warning, TEXT("Modified Health = %f"), AttributeSet->GetHealth())
+			Destroy();
+		}
+	}
+}
+
+void AEffectActor::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if(OtherActor)
+	{
+		UE_LOG(LogAura, Warning, TEXT("OnEndOverlap, hit actor: %s"), *OtherActor->GetName());
+	}
+}
+
+
---
+++ Source/Aura/Public/Actor/EffectActor.h
@@ -0,0 +1,37 @@
+﻿// Copyright XiaoYao.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EffectActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class AURA_API AEffectActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEffectActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	UFUNCTION()
+	void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+
+	
+	
+};
