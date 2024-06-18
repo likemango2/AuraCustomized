2a9dea4c0f2bcbddbfbcf1f201fb315ae207702f
likemango <330857910@qq.com>
2024-06-18 10:49:51 +0800

    实现单例NativeGameplayTag/DataAsset存储AttributeInfo

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright XiaoYao.
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for (const FAuraAttributeInfo& Element : AttributeInformation)
+	{
+		if(Element.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return Element;
+		}
+	}
+	if(bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find Info for AttributeTag [%s] on AttributeInfo [%s]."), *AttributeTag.ToString(),*GetNameSafe(this));
+	}
+	return FAuraAttributeInfo();
+}
