37b8278f06bf0cd54653190b92c20285192f18b9
likemango <330857910@qq.com>
2024-07-03 10:38:41 +0800

    AuraGameplayEffectContext/理解NetSerialize()原理/UClass和UScriptStruct

---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+	
+public:
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const override;
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess) override;
+	/** Creates a copy of this context, used to duplicate for later modifications */
+	virtual FAuraGameplayEffectContext* Duplicate() const override;
+
+	bool IsCriticalHit() const { return bIsCriticalHit;}
+	bool IsBlockedHit() const { return bIsBlockedHit;}
+
+	void SetCriticalHit(bool bInCriticalHit){ bIsCriticalHit = bInCriticalHit;}
+	void SetBlockedHit(bool bInBlockedHit) { bIsBlockedHit = bInBlockedHit;}
+	
+protected:
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+private:
+	
+};
+
+template<>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithCopy = true,		// Necessary so that TSharedPtr<FGameplayEffectContext> Data is copied around
+		WithNetSerializer = true,
+	};
+};
